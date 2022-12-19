055aa51b3a8404905b0d02d653d1dd97bac71f3e
Oredolapo-ayomide <ogunsijiayomide02@gmail.com>
2022-12-19 12:02:08 -0800

    task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - prints a string
+ * @str: string to print
+ *
+ * Return: nothing
+ */
+
+void puts_half(char *str)
 {
 	int i, len = _strlen(str);
 
