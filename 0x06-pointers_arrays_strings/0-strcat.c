e0176119b9e6f4df9b53a4829213aacaf983684e
Oredolapo-ayomide <ogunsijiayomide02@gmail.com>
2022-12-22 10:49:03 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Returns: destination
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int k = 0;
+
+	while (dest[i] != '\0') {
+		i++;
+	}
+
+	while (src[k] != '\0') {
+		dest[i] = src[k];
+		i++;
+		k++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
