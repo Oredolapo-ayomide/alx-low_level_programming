088f0eb15ee61e7c196ba069737e8e93c68a04a3
Oredolapo-ayomide <ogunsijiayomide02@gmail.com>
2022-12-23 01:46:41 -0800

    task 99

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * infinite_add - adds 2 numbers
+ * @n1: First number
+ * @n2: Second number
+ * @r:  Buffer the function that store result
+ * @size_r: buffer size
+ * Return: pointer to destination
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int
