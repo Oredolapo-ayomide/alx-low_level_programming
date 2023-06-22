aaa759ff1f1c806a6450fcb81baa431ec8da86c4
Oredolapo-ayomide <ogunsijiayomide02@gmail.com>
2023-06-21 23:40:11 -0700

    ayo

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isalpha - Return 1 if c is a letter. lowercase or uppercase
+ * @c: unsigned int value to be compared with ASCII value
+ *
+ * Return: 1 if its alphabet otherwise 0
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
